2511b6e339b4821f7b22c4170d23c55b3f012a3f
ilicina1 <ilhan.licina@outlook.com>
2021-05-28 09:47:09 +0200

    dovrsen prvi screen

--- lib/view/blog_screen/widgets/listview_custom.dart
+++ lib/view/blog_screen/widgets/listview_custom.dart
@@ -2,25 +2,39 @@ import 'package:flutter/material.dart';
 
 NotificationListener<OverscrollIndicatorNotification> listViewCustom(cardsList,
     [category]) {
-  var cat = 0;
+  var pom = 0;
   return NotificationListener<OverscrollIndicatorNotification>(
     // ignore: missing_return
     onNotification: (overscroll) {
       overscroll.disallowGlow();
     },
     child: ListView(
-      children: List<Widget>.generate(cardsList.length, (counter) {
-        if (cardsList[counter].category == category || category == null)
-          cat = 1;
-        return cardsList[counter].category == category || category == null
-            ? cat == 1
-                ? Padding(
-                    padding: const EdgeInsets.only(top: 40.0),
-                    child: cardsList[counter],
-                  )
-                : cardsList[counter]
-            : Container();
-      }),
+      children: List<Widget>.generate(
+        cardsList.length,
+        (counter) {
+          if (cardsList[counter].category == category || category == null) {
+            if (pom == 0) {
+              pom = 1;
+              return Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: cardsList[counter],
+              );
+            } else {
+              return cardsList[counter];
+            }
+          } else {
+            return Container();
+          }
+          // cardsList[counter].category == category || category == null
+          //     ? counter == 0
+          //         ? Padding(
+          //             padding: const EdgeInsets.only(top: 40.0),
+          //             child: cardsList[counter],
+          //           )
+          //         : cardsList[counter]
+          //     : Container();
+        },
+      ),
     ),
   );
 }
